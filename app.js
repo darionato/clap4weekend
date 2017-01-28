721e8dc15eb5bb975b130bb347c5e273014117b6
Dario Malfatti <dario.malfatti@gmail.com>
2017-01-28 17:13:17 +0100

    render the index page with pug template engine

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var app = express();
 
+
+
+// set the template engine
+app.set('view engine', 'pug');
+
+
+// bind the main url
 app.get('/', function (req, res) {
 
 
+  // require the countdown library
   let c = require('./countdown/countdown.js');
 
+  // create a new instance
   let r = new c.Countdown();
 
-  res.send(`Minute missing to clap your hands for weekend: ${r.getCountdown()}`);
+  // return the missing minutes
+  res.render('index', { 
+    content: `Minute missing to clap your hands for weekend: ${r.getCountdown()}`,
+    title: 'Clup 4 weekend'
+  });
 
 
 });
 
+
+// start the process
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
