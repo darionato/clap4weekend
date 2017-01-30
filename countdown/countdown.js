b9ef72eb84506230b16b5869152a043c109cab32
Dario Malfatti <dario.malfatti@gmail.com>
2017-01-30 21:25:08 +0100

    fix missing minutes calculate

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -7,6 +7,11 @@ class Countdown {
         let moment = require('moment');
         var friday = moment().day(5).hour(17).minute(30);
         var missing = friday.diff(moment(), 'minutes');
+
+        if (missing < 0) friday = friday.add(1, 'week');
+
+        missing = friday.diff(moment(), 'minutes');
+
         return missing;
 
     }
