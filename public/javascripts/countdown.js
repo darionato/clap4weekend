9b2f9552fe3a42435177c8b873402bc4083761a4
Dario Malfatti <dario.malfatti@gmail.com>
2017-02-05 20:28:11 +0100

    move the counter in the client side

---
+++ public/javascripts/countdown.js
@@ -0,0 +1,30 @@
+define(function() {
+
+
+    class Countdown {
+
+        constructor () {}
+
+        getCountdown () {
+            
+            var friday = moment().day(5).hour(17).minute(30);
+            var missing = friday.diff(moment(), 'minutes');
+
+            if (missing < 0) friday = friday.add(1, 'week');
+
+            missing = friday.diff(moment(), 'minutes');
+
+            return missing;
+
+        }
+
+    }
+
+    return new Countdown();
+
+});
+
+
+if (!(typeof(module) === 'undefined'))
+
+    module.exports.Countdown = Countdown
---
+++ public/javascripts/index.js
@@ -0,0 +1,18 @@
+require(['countdown'], function(countdown) {
+
+    var title = document.title;
+
+    startTimer = function() {
+
+        var c = countdown.getCountdown()
+
+        document.getElementById('counter').innerText = c;
+        document.title = `${c} - ${title}`;
+
+        setTimeout(startTimer, 5000);
+
+    }
+
+    startTimer();
+
+});
